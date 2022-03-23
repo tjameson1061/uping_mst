6769a50d3a91e62b59d07cb96600b01b143e726b
tomjameson <tjameson1061@icloud.com>
2022-03-23 00:05:59 +0000

    first commit

--- resources/js/src/libs/axios.js
+++ resources/js/src/libs/axios.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://portal.uping.co.uk/api/',
   timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 })
