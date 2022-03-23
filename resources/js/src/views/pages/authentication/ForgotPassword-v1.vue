15a8afb5611d84d67f02d7b43ca738dfae3ccb88
tomjameson <tjameson1061@icloud.com>
2022-03-23 16:53:33 +0000

    latest

--- resources/js/src/views/pages/authentication/ForgotPassword-v1.vue
+++ resources/js/src/views/pages/authentication/ForgotPassword-v1.vue
@@ -119,7 +119,7 @@ export default {
           //     variant: 'success',
           //   },
           // })
-            this.$http.post("/forgot-password", {email: this.user.email}).then(result => {
+            this.$http.post("/api/forgot-password", {email: this.user.email}).then(result => {
                 this.response = result.data;
               this.$toast({
                 component: ToastificationContent,
