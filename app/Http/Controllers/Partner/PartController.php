0b3c09bafb9c4024084789fe015efe7561f60c6d
tomjameson <tjameson1061@icloud.com>
2022-03-23 00:39:51 +0000

    dashboard

--- app/Http/Controllers/Partner/PartController.php
+++ app/Http/Controllers/Partner/PartController.php
@@ -55,7 +55,7 @@ class PartController extends Controller
 
     public function getUserData($id)
     {
-        $user = User::find($id)->with(['company'])->first();
+        $user = User::find($id)->with('company')->first();
 
 
         return Response::json(['user' => $user], 200);
