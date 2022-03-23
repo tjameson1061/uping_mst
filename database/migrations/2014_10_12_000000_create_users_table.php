40b107be8af260a8acbb5459ac066ffcf36c21aa
tomjameson <tjameson1061@icloud.com>
2022-03-23 09:50:11 +0000

    latest

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('username');
-            $table->string('avatar')->default('default.jpg');
+            $table->longText('avatar')->default('default.jpg');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
