2c37317eb231dcab2504529c4687b38c3ead96bc
tomjameson <tjameson1061@icloud.com>
2022-03-24 11:50:50 +0000

    latest

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,23 +13,25 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('username');
-            $table->longText('avatar')->nullable();;
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('username');
+                $table->longText('avatar')->nullable();;
+                $table->string('email')->unique();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('password');
+                $table->rememberToken();
 
-            $table->integer('status');
-            $table->integer('is_admin');
-            $table->string('role');
-            $table->string('ability')->nullable();
+                $table->integer('status');
+                $table->integer('is_admin');
+                $table->string('role');
+                $table->string('ability')->nullable();
 
-            $table->timestamps();
-        });
+                $table->timestamps();
+            });
+        }
     }
 
     /**
