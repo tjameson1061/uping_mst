95fc93a35bced13f03094d647e4b16264027ec9a
tomjameson <tjameson1061@icloud.com>
2022-04-30 21:46:52 +0100

    latest

--- resources/js/src/views/apps/partnerdashboard/offer/e-commerce-shop/ECommerceShop.vue
+++ resources/js/src/views/apps/partnerdashboard/offer/e-commerce-shop/ECommerceShop.vue
@@ -106,7 +106,7 @@
                     :alt="`${offer.name}-${offer.id}`"
                     fluid
                     class="card-img-top p-3"
-                    :src="require('@/assets/images/offer_logo/magic-lend.png')"
+                    :src="require('@/assets/images/offer_logo/loanie-logo.png')"
 
             />
 
