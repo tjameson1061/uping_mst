12237559a0180d7f7ad6020fea4ea8a7494da65a
tomjameson <tjameson1061@icloud.com>
2022-04-30 22:41:26 +0100

    first commit 1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,6 +23,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
+
     /**
      * The application's route middleware groups.
      * @var array
