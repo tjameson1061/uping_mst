d7d8562c3510b05373156ba982334d5b29fc6481
Tom Jameson <tomjameson@giga.local>
2023-04-23 15:48:53 +0100

    axios

--- resources/js/src/libs/axios.js
+++ resources/js/src/libs/axios.js
@@ -6,8 +6,8 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  // baseURL: 'https://portal.uping.co.uk/api/',
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://portal.uping.co.uk/api/',
+  // baseURL: 'http://127.0.0.1:8000/api/',
   timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 })
